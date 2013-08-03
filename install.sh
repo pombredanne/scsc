a7687b543dadc43c63b44536d2aecabcbd490fb7
myfreeweb <floatboth@me.com>
2013-08-03 16:12:04 +0400

    sudo all the things

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ if [[ ! -d /opt/scsc ]]; then
   yes | sudo apt-get install git
 
   echo ">>> Installing Chef"
-  curl -L https://www.opscode.com/chef/install.sh | bash
+  curl -L https://www.opscode.com/chef/install.sh | sudo bash
 
   echo ">>> Downloading SCSC"
-  git clone https://github.com/myfreeweb/scsc.git /opt/scsc
+  sudo git clone https://github.com/myfreeweb/scsc.git /opt/scsc
 
   echo ">>> Running Chef"
   cd /opt/scsc
-  chef-solo -c solo.rb -j nodes/scsc.json
+  sudo chef-solo -c solo.rb -j nodes/scsc.json
 else
   echo "SCSC is already installed!"
   echo "Use scsc-update to update it."
