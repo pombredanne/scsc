424a2bfa8f0b412a050425982bc71ecb9ed7acfd
myfreeweb <floatboth@me.com>
2013-08-15 02:16:35 +0400

    moar dns stuff

--- site-cookbooks/dnsmasq/attributes/default.rb
+++ site-cookbooks/dnsmasq/attributes/default.rb
@@ -1,4 +1,9 @@
-default["dnsmasq"]["opennic_server_1"] = "213.138.101.252"
-default["dnsmasq"]["opennic_server_2"] = "185.19.105.14"
-default["dnsmasq"]["opennic_domains"] = %w(.bbs .dyn .free .fur .geek .gopher .indy .ing .micro .neo .null .oss .oz .parody)
+default["dnsmasq"]["opennic_servers"] = %w(213.138.101.252 185.19.105.14)
+default["dnsmasq"]["opennic_domains"] = %w(.bbs .dyn .free .fur .geek .gopher .indy .ing .micro .neo .null .oss .oz .parody .pirate)
+
+default["dnsmasq"]["new_nations_servers"] = %w(178.254.31.11 176.10.111.206)
+default["dnsmasq"]["new_nations_domains"] = %w(.uu .ti .te .ku)
+
+default["dnsmasq"]["42_servers"] = %w(81.93.248.69 81.93.248.68 91.194.60.196 193.17.192.53 109.235.51.12 85.17.236.67)
+
 default["dnsmasq"]["dnscrypt_port"] = "2053"
