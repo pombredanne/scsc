04a46d330b37537ce6ed44265a924a2618b20b56
myfreeweb <floatboth@me.com>
2013-09-09 20:45:12 +0400

    Use torproject repo for tor

--- site-cookbooks/tor/metadata.rb
+++ site-cookbooks/tor/metadata.rb
@@ -1,1 +1,3 @@
+depends "apt"
+
 supports "ubuntu"
--- site-cookbooks/tor/recipes/default.rb
+++ site-cookbooks/tor/recipes/default.rb
@@ -1,4 +1,16 @@
-package "tor"
+include_recipe "apt"
+
+apt_repository "tor" do
+  uri "http://deb.torproject.org/torproject.org"
+  distribution node["lsb"]["codename"]
+  components ["main"]
+  keyserver "keys.gnupg.net"
+  key "886DDD89"
+end
+
+package "tor" do
+  action :upgrade
+end
 
 service "tor" do
   supports [:restart, :reload, :status]
