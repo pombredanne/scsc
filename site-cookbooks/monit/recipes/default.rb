d5d81f86dae45614f3424ddd838e04f2be572a77
myfreeweb <floatboth@me.com>
2013-10-09 13:44:06 +0400

    notify monit restart

--- site-cookbooks/monit/recipes/default.rb
+++ site-cookbooks/monit/recipes/default.rb
@@ -4,11 +4,13 @@ end
 
 template "/etc/monit/monitrc" do
   source "monitrc.erb"
+  notifies :restart, "service[monit]"
 end
 
 %w(znc).each do |rc|
   template "/etc/monit/conf.d/#{rc}" do
     source "#{rc}.erb"
+    notifies :restart, "service[monit]"
   end
 end
 
