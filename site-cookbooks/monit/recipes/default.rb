74272c81279ab67594bd9c503c1f336916823dab
myfreeweb <floatboth@me.com>
2013-10-09 16:49:48 +0400

    monit dnscrypt

--- site-cookbooks/monit/recipes/default.rb
+++ site-cookbooks/monit/recipes/default.rb
@@ -13,17 +13,10 @@ pids = {
   "apache2" => "/var/run/apache2.pid",
   "privoxy" => "/var/run/privoxy.pid",
   "tor-dns-proxy" => "/var/run/tor-dns-proxy.pid",
+  "dnscrypt-proxy-primary" => "/var/run/dnscrypt-proxy-primary.pid",
+  "dnscrypt-proxy-secondary" => "/var/run/dnscrypt-proxy-secondary.pid",
+  "dnscrypt-proxy-opendns" => "/var/run/dnscrypt-proxy-opendns.pid",
   "socat-freenode" => "/var/run/socat-freenode.pid"
-}
-
-starts = {
-  "tor-dns-proxy"  => "/usr/sbin/service tor-dns-proxy start",
-  "socat-freenode" => "/usr/sbin/service socat-freenode start"
-}
-
-stops = {
-  "tor-dns-proxy"  => "/usr/sbin/service tor-dns-proxy stop",
-  "socat-freenode" => "/usr/sbin/service socat-freenode stop"
 }
 
 hosts = {}
@@ -32,6 +25,9 @@ ports = {
   "znc" => node["znc"]["port"],
   "i2p" => 7657,
   "dnsmasq" => 53,
+  "dnscrypt-proxy-primary" => 2053,
+  "dnscrypt-proxy-secondary" => 3053,
+  "dnscrypt-proxy-opendns" => 4053,
   "privoxy" => node["privoxy"]["port"],
   "socat-freenode" => node["znc"]["socat-freenode-port"],
   "tor" => node["tor"]["socks-port"],
@@ -39,25 +35,34 @@ ports = {
 }
 
 types = {
-  "dnsmasq" => "udp"
+  "dnsmasq" => "udp",
+  "dnscrypt-proxy-primary" => "udp",
+  "dnscrypt-proxy-secondary" => "udp",
+  "dnscrypt-proxy-opendns" => "udp"
 }
 
 protos = {
   "dnsmasq" => "dns",
+  "dnscrypt-proxy-primary" => "dns",
+  "dnscrypt-proxy-secondary" => "dns",
+  "dnscrypt-proxy-opendns" => "dns",
   "privoxy" => "default",
   "socat-freenode" => "default",
   "tor" => "default",
   "tor-dns-proxy" => "default"
 }
 
-%w(apache2 znc i2p dnsmasq privoxy tor tor-dns-proxy socat-freenode).each do |rc|
+%w(apache2
+dnsmasq dnscrypt-proxy-primary dnscrypt-proxy-secondary dnscrypt-proxy-opendns
+i2p tor tor-dns-proxy socat-freenode privoxy
+znc).each do |rc|
   template "/etc/monit/conf.d/#{rc}" do
     source "process.erb"
     variables({
       :process  => rc,
       :pidfile  => pids[rc]   || "/var/run/#{rc}/#{rc}.pid",
-      :start    => starts[rc] || "/etc/init.d/#{rc} start",
-      :stop     => stops[rc]  || "/etc/init.d/#{rc} stop",
+      :start    => "/usr/sbin/service #{rc} start",
+      :stop     => "/usr/sbin/service #{rc} stop",
       :host     => hosts[rc]  || "localhost",
       :port     => ports[rc]  || 80,
       :type     => types[rc]  || "tcp",
