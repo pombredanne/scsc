ba54cbd34c39617a28f083fbcf787a8fc290b791
myfreeweb <floatboth@me.com>
2013-10-11 17:22:37 +0400

    use ruby_block for poche db path

--- site-cookbooks/poche/recipes/default.rb
+++ site-cookbooks/poche/recipes/default.rb
@@ -14,7 +14,18 @@ bash "install poche" do
   chown www-data:data cache
   composer install
   cp install/poche.sqlite /data/poche.sqlite
-  cat inc/poche/config.inc.php.new | sed -e "s/'STORAGE_SQLITE', ROOT . '\/db\/poche.sqlite'/'STORAGE_SQLITE', '\/data\/poche.sqlite'/" > inc/poche/config.inc.php
+  cp inc/poche/config.inc.php.new inc/poche/config.inc.php
   EOH
   creates "/data/poche.sqlite"
+end
+
+poche_conf = ::File.join(node["poche"]["root"], "inc/poche/config.inc.php")
+
+ruby_block "change poche db path" do
+  block do
+    f = Chef::Util::FileEdit.new(poche_conf)
+    f.search_file_replace_line /define \('STORAGE_SQLITE.*/, "define ('STORAGE_SQLITE', '/data/poche.sqlite');"
+    f.write_file
+  end
+  not_if { ::File.read(poche_conf).include? "/data/poche.sqlite" }
 end
