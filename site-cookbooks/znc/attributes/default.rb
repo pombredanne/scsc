ac3381efc46ef91370775e8a1bc13545a6216a3b
myfreeweb <floatboth@me.com>
2013-10-14 19:32:00 +0400

    add onionnet to znc

--- site-cookbooks/znc/attributes/default.rb
+++ site-cookbooks/znc/attributes/default.rb
@@ -1,4 +1,8 @@
 default["znc"]["user"] = "znc"
 default["znc"]["group"] = "znc"
-default["znc"]["port"] = 6697
-default["znc"]["socat-freenode-port"] = 6696
+default["znc"]["port"] = 6667
+default["znc"]["irc2p-port"] = 6668
+default["znc"]["freenode-address"] = '$(dig +short irc.tor.freenode.net cname | sed -E "s/([^\.]+\.[^\.]+)\./\1/")'
+default["znc"]["socat-freenode-port"] = 6669
+default["znc"]["onionnet-address"] = "ftwircdwyhghzw4i.onion"
+default["znc"]["socat-onionnet-port"] = 6670
--- site-cookbooks/znc/recipes/default.rb
+++ site-cookbooks/znc/recipes/default.rb
@@ -40,11 +40,20 @@ template "/etc/init/znc.conf" do
 end
 
 template "/etc/init/socat-freenode.conf" do
-  source "socat-freenode.conf.erb"
+  source "socat.conf.erb"
+  variables(:network => "freenode", :listen_port => node["znc"]["socat-freenode-port"],
+            :network_address => node["znc"]["freenode-address"])
   mode "0755"
 end
 
-%w(znc socat-freenode).each do |srv|
+template "/etc/init/socat-onionnet.conf" do
+  source "socat.conf.erb"
+  variables(:network => "onionnet", :listen_port => node["znc"]["socat-onionnet-port"],
+            :network_address => node["znc"]["onionnet-address"])
+  mode "0755"
+end
+
+%w(znc socat-freenode socat-onionnet).each do |srv|
   service srv do
     provider Chef::Provider::Service::Upstart
     supports [:restart, :reload, :status]
